97a3fe76e179a12885f7a98977652b7af4265ae9
Emmanuel Farrar <6923873+emmanuelfarrar@users.noreply.github.com>
2020-01-26 04:25:19 +0300

    Slight modifications for U02A1

--- app/src/test/java/edu/capella/it4782/simplecalculator/btnClickMathTest.java
+++ app/src/test/java/edu/capella/it4782/simplecalculator/btnClickMathTest.java
@@ -5,17 +5,19 @@ import android.view.View;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import static org.junit.Assert.*;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.*;
 
 import static org.junit.Assert.*;
 
-@RunWith(MockitoJUnitRunner.class)
+
 public class btnClickMathTest {
 
-    @Mock
-    View mockView;
+    MainActivity mainAct = Mockito.mock(MainActivity.class);
+
+
 
     @Before
     public void setUp() throws Exception {
@@ -26,21 +28,13 @@ public class btnClickMathTest {
     }
 
     @Test
-    public void buttononClickTest() {
+    public void addBtnTest() {
 
-        // Do I need to use my own variables
-        float num1 = 0;
-        float num2 = 0;
-        float result;
 
-        MainActivity calc = new MainActivity();
+        assertEquals(5.0f, mainAct.btnAdd);
 
-        // Not sure how to structure this part
-
-        assertTrue(calc.buttononClick(R.id.btnAdd));
-        assertTrue(calc.buttononClick(R.id.btnSub));
-        assertTrue(calc.buttononClick(R.id.btnMult));
-        assertTrue(calc.buttononClick(R.id.btnDiv));
 
     }
+
+
 }
